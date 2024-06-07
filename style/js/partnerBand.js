5dfc6afeddde28ee45d542223fff36d09468ae09
OvBd1 <y.bontrond@ecole-ipssi.net>
2024-06-07 15:34:18 +0200

    :sparkles: Fix général

--- style/js/partnerBand.js
+++ style/js/partnerBand.js
@@ -1,2 +1,10 @@
-var copy = document.querySelector(".logos-slide").cloneNode(true);
-document.querySelector(".logos").appendChild(copy);
+function marqueelike() {
+  document.querySelectorAll('.messageDefilant').forEach((element) => {
+    var texte = element.innerHTML;
+    element.innerHTML = '<div><span>' + texte + '</span><span>' + texte + '</span></div>';
+  });
+}
+
+window.addEventListener('load', function() {
+  marqueelike();
+});
